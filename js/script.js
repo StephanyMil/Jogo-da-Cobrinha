1e716250385e0d45e5d0774d5aa00db58526d465
Stephany Milhomem <stephanymilhomem25@gmail.com>
2023-07-28 15:17:15 -0300

    Esqueleto do projeto_Parte 1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+
+const score = document.querySelector(".score--value")
+const finalScore = document.querySelector(".final-score > span")
+const menu = document.querySelector(".menu-screen")
+const buttonPlay = document.querySelector(".btn-play")
+
+const audio = new Audio("../assets/audio.mp3")
