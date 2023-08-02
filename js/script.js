2b804c3a6616be86bec14972e1c29bc48ef429d6
Aline <aline.coelho@discente.ufg.br>
2023-08-01 21:18:51 -0300

    Add function drawGrid

--- js/script.js
+++ js/script.js
@@ -91,7 +91,22 @@ const moveSnake = () => {
 
 // Aline
 const drawGrid = () => {
-    
+    ctx.lineWidth = 1
+    ctx.strokeStyle = "#191919"
+
+    for (let i = 30; i < canvas.width; i += 30) {
+        ctx.beginPath()
+        ctx.lineTo(i, 0)
+        ctx.lineTo(i, 600)
+        ctx.stroke()
+
+        ctx.beginPath()
+        
+        ctx.lineTo(0, i)
+        ctx.lineTo(600, i)
+
+        ctx.stroke()
+    }
 }
 
 // Stephany
